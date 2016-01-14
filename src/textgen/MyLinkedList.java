613cfb72974a68d3a94cd00b5e59ec5defceddac
Ranjanabh Bahukhandi <ranjanabhb@gmail.com>
2016-01-14 15:29:30 -0500

    Module 3

--- src/textgen/MyLinkedList.java
+++ src/textgen/MyLinkedList.java
@@ -10,91 +10,157 @@ import java.util.AbstractList;
  * @param <E> The type of the elements stored in the list
  */
 public class MyLinkedList<E> extends AbstractList<E> {
-	LLNode<E> head;
-	LLNode<E> tail;
-	int size;
+LLNode<E> head;
+LLNode<E> tail;
+int size;
 
-	/** Create a new empty LinkedList */
-	public MyLinkedList() {
-		// TODO: Implement this method
-	}
-
-	/**
-	 * Appends an element to the end of the list
-	 * @param element The element to add
-	 */
-	public boolean add(E element ) 
-	{
-		// TODO: Implement this method
-		return false;
-	}
-
-	/** Get the element at position index 
-	 * @throws IndexOutOfBoundsException if the index is out of bounds. */
-	public E get(int index) 
-	{
-		// TODO: Implement this method.
-		return null;
-	}
-
-	/**
-	 * Add an element to the list at the specified index
-	 * @param The index where the element should be added
-	 * @param element The element to add
-	 */
-	public void add(int index, E element ) 
-	{
-		// TODO: Implement this method
-	}
-
-
-	/** Return the size of the list */
-	public int size() 
-	{
-		// TODO: Implement this method
-		return -1;
-	}
-
-	/** Remove a node at the specified index and return its data element.
-	 * @param index The index of the element to remove
-	 * @return The data element removed
-	 * @throws IndexOutOfBoundsException If index is outside the bounds of the list
-	 * 
-	 */
-	public E remove(int index) 
-	{
-		// TODO: Implement this method
-		return null;
-	}
-
-	/**
-	 * Set an index position in the list to a new element
-	 * @param index The index of the element to change
-	 * @param element The new element
-	 * @return The element that was replaced
-	 * @throws IndexOutOfBoundsException if the index is out of bounds.
-	 */
-	public E set(int index, E element) 
-	{
-		// TODO: Implement this method
-		return null;
-	}   
+/** Create a new empty LinkedList */
+public MyLinkedList() {
+    head = new LLNode<E>(null);
+    tail = new LLNode<E>(null);
+    head.next = tail;
+    tail.prev = head;
+    size = 0;
 }
+
+/**
+ * Appends an element to the end of the list
+ * @param element The element to add
+ */
+public boolean add(E element) 
+{
+    isElementIsNotNull(element);
+    
+    LLNode<E> newNode = new LLNode<E>(element);
+    if(0 == this.size()) {
+        head.next = newNode;
+        newNode.prev = head;
+    } else {
+        newNode.prev = tail.prev;
+        tail.prev.next = newNode;
+    }
+    
+    newNode.next = tail;
+    tail.prev = newNode;    
+    ++size;
+    return true;
+}
+
+/** Get the element at position index 
+ * @throws IndexOutOfBoundsException if the index is out of bounds. */
+public E get(int index) 
+{   
+    if(this.isIndexValidToRead(index)) {
+        return this.getNode(index).data;
+    } else {
+        throw new IndexOutOfBoundsException("Requested index out of bounds!");
+    }
+}
+
+/**
+ * Add an element to the list at the specified index
+ * @param The index where the element should be added
+ * @param element The element to add
+ */
+public void add(int index, E element) 
+{
+    isElementIsNotNull(element);
+    
+    if(this.isIndexValidToWrite(index)) {
+        LLNode<E> tmpNode = this.getNode(index);
+        LLNode<E> newNode = new LLNode<E>(element);
+        newNode.next = tmpNode;
+        newNode.prev = tmpNode.prev;
+        tmpNode.prev.next = newNode;
+        tmpNode.prev = newNode;
+        ++size;
+    } else {
+        throw new IndexOutOfBoundsException("Requested index out of bounds!");
+    }
+}
+
+/** Return the size of the list */
+public int size() 
+{
+    return this.size;
+}
+
+/** Remove a node at the specified index and return its data element.
+ * @param index The index of the element to remove
+ * @return The data element removed
+ * @throws IndexOutOfBoundsException If index is outside the bounds of the list
+ * 
+ */
+public E remove(int index) 
+{
+    if(this.isIndexValidToRead(index)) {
+        LLNode<E> tmpNode = this.getNode(index);
+        tmpNode.prev.next = tmpNode.next;
+        tmpNode.next.prev = tmpNode.prev;
+        tmpNode.next = null;
+        tmpNode.prev = null;
+        --size;
+        return tmpNode.data;
+    } else {        
+        throw new IndexOutOfBoundsException("Requested index out of bounds!");
+    }
+}
+
+/**
+ * Set an index position in the list to a new element
+ * @param index The index of the element to change
+ * @param element The new element
+ * @return The element that was replaced
+ * @throws IndexOutOfBoundsException if the index is out of bounds.
+ */
+public E set(int index, E element) 
+{
+    isElementIsNotNull(element);
+    E oldElement = this.get(index);
+    this.getNode(index).data = element;
+    return oldElement;
+}   
+
+private boolean isIndexValidToRead(int index) {
+    return !(index < 0 || index >= this.size());
+}
+
+private boolean isIndexValidToWrite(int index) {
+    return !(index < 0 || index > this.size());
+}
+
+private LLNode<E> getNode(int index) {
+    int currIdx = 0;
+    LLNode<E> runner = this.head.next;
+    while(currIdx < index) {
+        runner = runner.next;
+        ++currIdx;
+    }
+    return runner;
+}
+
+private void isElementIsNotNull(Object obj) {
+    if(obj == null) {
+        throw new NullPointerException();
+    }    
+}
+
+} // class MyLinkedList
 
 class LLNode<E> 
 {
-	LLNode<E> prev;
-	LLNode<E> next;
-	E data;
+LLNode<E> prev;
+LLNode<E> next;
+E data;
 
-	// TODO: Add any other methods you think are useful here
-	// E.g. you might want to add another constructor
+// TODO: Add any other methods you think are useful here
+// E.g. you might want to add another constructor
 
-	public LLNode(E e) 
-	{
-		this.data = e;
-		this.prev = null;
-		this.next = null;
-	}
+public LLNode(E e) 
+{
+    this.data = e;
+    this.prev = null;
+    this.next = null;
+}
 
 }
--- src/textgen/MyLinkedListTester.java
+++ src/textgen/MyLinkedListTester.java
@@ -16,147 +16,218 @@ import org.junit.Test;
  */
 public class MyLinkedListTester {
 
-	private static final int LONG_LIST_LENGTH =10; 
+private static final int LONG_LIST_LENGTH = 10000; 
 
-	MyLinkedList<String> shortList;
-	MyLinkedList<Integer> emptyList;
-	MyLinkedList<Integer> longerList;
-	MyLinkedList<Integer> list1;
-	
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-		// Feel free to use these lists, or add your own
-	    shortList = new MyLinkedList<String>();
-		shortList.add("A");
-		shortList.add("B");
-		emptyList = new MyLinkedList<Integer>();
-		longerList = new MyLinkedList<Integer>();
-		for (int i = 0; i < LONG_LIST_LENGTH; i++)
-		{
-			longerList.add(i);
-		}
-		list1 = new MyLinkedList<Integer>();
-		list1.add(65);
-		list1.add(21);
-		list1.add(42);
-		
-	}
+MyLinkedList<String> shortList;
+MyLinkedList<Integer> emptyList;
+MyLinkedList<Integer> longerList;
+MyLinkedList<Integer> list1;
 
-	
-	/** Test if the get method is working correctly.
-	 */
-	/*You should not need to add much to this method.
-	 * We provide it as an example of a thorough test. */
-	@Test
-	public void testGet()
-	{
-		//test empty list, get should throw an exception
-		try {
-			emptyList.get(0);
-			fail("Check out of bounds");
-		}
-		catch (IndexOutOfBoundsException e) {
-			
-		}
-		
-		// test short list, first contents, then out of bounds
-		assertEquals("Check first", "A", shortList.get(0));
-		assertEquals("Check second", "B", shortList.get(1));
-		
-		try {
-			shortList.get(-1);
-			fail("Check out of bounds");
-		}
-		catch (IndexOutOfBoundsException e) {
-		
-		}
-		try {
-			shortList.get(2);
-			fail("Check out of bounds");
-		}
-		catch (IndexOutOfBoundsException e) {
-		
-		}
-		// test longer list contents
-		for(int i = 0; i<LONG_LIST_LENGTH; i++ ) {
-			assertEquals("Check "+i+ " element", (Integer)i, longerList.get(i));
-		}
-		
-		// test off the end of the longer array
-		try {
-			longerList.get(-1);
-			fail("Check out of bounds");
-		}
-		catch (IndexOutOfBoundsException e) {
-		
-		}
-		try {
-			longerList.get(LONG_LIST_LENGTH);
-			fail("Check out of bounds");
-		}
-		catch (IndexOutOfBoundsException e) {
-		}
-		
-	}
-	
-	
-	/** Test removing an element from the list.
-	 * We've included the example from the concept challenge.
-	 * You will want to add more tests.  */
-	@Test
-	public void testRemove()
-	{
-		int a = list1.remove(0);
-		assertEquals("Remove: check a is correct ", 65, a);
-		assertEquals("Remove: check element 0 is correct ", (Integer)21, list1.get(0));
-		assertEquals("Remove: check size is correct ", 2, list1.size());
-		
-		// TODO: Add more tests here
-	}
-	
-	/** Test adding an element into the end of the list, specifically
-	 *  public boolean add(E element)
-	 * */
-	@Test
-	public void testAddEnd()
-	{
-        // TODO: implement this test
-		
-	}
+/**
+ * @throws java.lang.Exception
+ */
+@Before
+public void setUp() throws Exception {
+    // Feel free to use these lists, or add your own
+    shortList = new MyLinkedList<String>();
+    shortList.add("A");
+    shortList.add("B");
+    emptyList = new MyLinkedList<Integer>();
+    longerList = new MyLinkedList<Integer>();
+    for (int i = 0; i < LONG_LIST_LENGTH; i++)
+    {
+        longerList.add(i);
+    }
+    list1 = new MyLinkedList<Integer>();
+    list1.add(65);
+    list1.add(21);
+    list1.add(42);
+}
 
-	
-	/** Test the size of the list */
-	@Test
-	public void testSize()
-	{
-		// TODO: implement this test
-	}
+/** Test if the get method is working correctly.
+ */
+/*You should not need to add much to this method.
+ * We provide it as an example of a thorough test. */
+@Test
+public void testGet()
+{
+    //test empty list, get should throw an exception
+    try {
+        emptyList.get(0);
+        fail("Check out of bounds");
+    }
+    catch (IndexOutOfBoundsException e) {
 
-	
-	
-	/** Test adding an element into the list at a specified index,
-	 * specifically:
-	 * public void add(int index, E element)
-	 * */
-	@Test
-	public void testAddAtIndex()
-	{
-        // TODO: implement this test
-		
-	}
-	
-	/** Test setting an element in the list */
-	@Test
-	public void testSet()
-	{
-	    // TODO: implement this test
-	    
-	}
-	
-	
-	// TODO: Optionally add more test methods.
-	
+    }
+
+    // test short list, first contents, then out of bounds
+    assertEquals("Check first", "A", shortList.get(0));
+    assertEquals("Check second", "B", shortList.get(1));
+
+    try {
+        shortList.get(-1);
+        fail("Check out of bounds");
+    }
+    catch (IndexOutOfBoundsException e) {
+    }
+    
+    try {
+        shortList.get(2);
+        fail("Check out of bounds");
+    }
+    catch (IndexOutOfBoundsException e) {
+    }
+    
+    // test longer list contents
+    for(int i = 0; i<LONG_LIST_LENGTH; i++ ) {
+        assertEquals("Check "+i+ " element", (Integer)i, longerList.get(i));
+    }
+
+    // test off the end of the longer array
+    try {
+        longerList.get(-1);
+        fail("Check out of bounds");
+    }
+    catch (IndexOutOfBoundsException e) {
+    }
+    
+    try {
+        longerList.get(LONG_LIST_LENGTH);
+        fail("Check out of bounds");
+    }
+    catch (IndexOutOfBoundsException e) {
+    }
+
+}
+
+/** Test removing an element from the list.
+ * We've included the example from the concept challenge.
+ * You will want to add more tests.  */
+@Test
+public void testRemove()
+{
+    int a = list1.remove(0);
+    assertEquals("Remove: check a is correct ", 65, a);
+    assertEquals("Remove: check element 0 is correct ", (Integer)21, list1.get(0));
+    assertEquals("Remove: check size is correct ", 2, list1.size());
+    list1.add(10);
+    assertEquals("Remove: check element 2 is correct ", (Integer)10, list1.get(2));
+    assertEquals("Remove: check size is correct ", 3, list1.size());
+    list1.add(2, 15);
+    assertEquals("Remove: check add at indx", 4, list1.size());
+    assertEquals("Remove: check element 2 is correct ", (Integer)15, list1.get(2));
+    
+    try {
+        emptyList.remove(0);
+        fail("Check out of bounds");
+    }
+    catch (IndexOutOfBoundsException e) {
+
+    }    
+    
+    emptyList.add(10);    
+    assertEquals("Remove: check element 0 is correct ", (Integer)10, emptyList.get(0));
+    assertEquals("Remove: check size is correct ", 1, emptyList.size());
+    int b = emptyList.remove(0);
+    assertEquals("Remove: check a is correct ", 10, b);
+    assertEquals("Remove: check size is correct ", 0, emptyList.size());
+    emptyList.add(20);    
+    assertEquals("Remove: check element 0 is correct ", (Integer)20, emptyList.get(0));
+    assertEquals("Remove: check size is correct ", 1, emptyList.size());    
+
+    MyLinkedList<Integer> lst = new MyLinkedList<Integer>();
+    lst.add(0, 1);
+    lst.remove(0);
+    lst.add(0, 1);    
+}
+
+/** Test adding an element into the end of the list, specifically
+ *  public boolean add(E element)
+ * */
+@Test
+public void testAddEnd()
+{
+    longerList.add(LONG_LIST_LENGTH, 666);
+    assertEquals("Check end", (Integer)666, longerList.get(LONG_LIST_LENGTH));
+    assertEquals("Check Size", (LONG_LIST_LENGTH + 1), longerList.size());    
+}
+/** Test the size of the list */
+@Test
+public void testSize()
+{
+    assertEquals("Check Size", 0, emptyList.size());
+    assertEquals("Check Size", 2, shortList.size());
+    assertEquals("Check Size", LONG_LIST_LENGTH, longerList.size());
+    
+    for (int i = 0; i < LONG_LIST_LENGTH; i++)
+    {
+        emptyList.add(i);
+    }
+    assertEquals("Check Size after add()", LONG_LIST_LENGTH, emptyList.size());
+    
+    for (int i = 0; i < LONG_LIST_LENGTH; i++)
+    {
+        emptyList.remove(0);
+    }
+    assertEquals("Check Size after remove()", 0, emptyList.size());
+}
+
+/** Test adding an element into the list at a specified index,
+ * specifically:
+ * public void add(int index, E element)
+ * */
+@Test
+public void testAddAtIndex()
+{
+    try {
+        emptyList.add(1, 10);
+        fail("Check out of bounds");
+    }
+    catch (IndexOutOfBoundsException e) {
+    }
+
+    emptyList.add(0, 10);
+    assertEquals("Check first", (Integer)10, emptyList.get(0));
+    assertEquals("Check Size", 1, emptyList.size());
+
+    longerList.add((LONG_LIST_LENGTH/2), 123);
+    assertEquals("Check middle", (Integer)123, longerList.get(LONG_LIST_LENGTH/2));
+    assertEquals("Check Size", (LONG_LIST_LENGTH + 1), longerList.size());    
+    
+    longerList.add(LONG_LIST_LENGTH, 666);
+    assertEquals("Check end", (Integer)666, longerList.get(LONG_LIST_LENGTH));
+    assertEquals("Check Size", (LONG_LIST_LENGTH + 2), longerList.size());   
+}
+
+/** Test setting an element in the list */
+@Test
+public void testSet()
+{
+    try {
+        emptyList.set(0, null);
+        fail("Check for null object");
+    }
+    catch (NullPointerException e) {
+    }
+
+    try {
+        emptyList.set(0, 10);
+        fail("Check for index out of bounds");
+    }
+    catch (IndexOutOfBoundsException e) {
+    }    
+    
+    int number = longerList.get(0);
+    assertEquals("Check first", (Integer)number, longerList.set(0, (number*2)));
+    assertEquals("Check first", (Integer)(number*2), longerList.get(0));    
+    
+    number = longerList.get(LONG_LIST_LENGTH/2);
+    assertEquals("Check middle", (Integer)number, longerList.set(LONG_LIST_LENGTH/2, (number*2)));
+    assertEquals("Check middle", (Integer)(number*2), longerList.get(LONG_LIST_LENGTH/2));
+    
+    number = longerList.get(LONG_LIST_LENGTH-1);
+    assertEquals("Check last", (Integer)number, longerList.set(LONG_LIST_LENGTH-1, (number)));
+    assertEquals("Check last", (Integer)(number), longerList.get(LONG_LIST_LENGTH-1));
+}
+
 }
