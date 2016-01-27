96e7fcd6193e9f9f217931396909c99be70970bb
Ranjanabh Bahukhandi <ranjanabhb@gmail.com>
2016-01-26 23:38:28 -0500

    Week4 Submisson

--- src/spelling/DictionaryLL.java
+++ src/spelling/DictionaryLL.java
@@ -6,37 +6,46 @@ import java.util.LinkedList;
  * A class that implements the Dictionary interface using a LinkedList
  *
  */
-public class DictionaryLL implements Dictionary 
-{
+public class DictionaryLL implements Dictionary {
 
-	private LinkedList<String> dict;
-	
-    // TODO: Add a constructor
+private LinkedList<String> dict;
 
+public DictionaryLL() {
+    dict = new LinkedList<String>();
+}
 
-    /** Add this word to the dictionary.  Convert it to lowercase first
-     * for the assignment requirements.
-     * @param word The word to add
-     * @return true if the word was added to the dictionary 
-     * (it wasn't already there). */
-    public boolean addWord(String word) {
-    	// TODO: Implement this method
+/** Add this word to the dictionary.  Convert it to lowercase first
+ * for the assignment requirements.
+ * @param word The word to add
+ * @return true if the word was added to the dictionary 
+ * (it wasn't already there). */
+public boolean addWord(String word) {    
+    if(!this.isWord(word.toLowerCase())) {
+        return dict.add(word.toLowerCase());
+    } {
         return false;
     }
+}
 
+/** Return the number of words in the dictionary */
+public int size() {    
+    return dict.size();
+}
 
-    /** Return the number of words in the dictionary */
-    public int size()
-    {
-        // TODO: Implement this method
-        return 0;
-    }
+/** Is this a word according to this dictionary? */
+public boolean isWord(String s) {    
+    return dict.contains(s.toLowerCase());
+}
 
-    /** Is this a word according to this dictionary? */
-    public boolean isWord(String s) {
-        //TODO: Implement this method
-        return false;
-    }
-
+public static void main(String [] args) {
+    DictionaryLL smallDict = new DictionaryLL();
     
+    smallDict.addWord("Hello");
+    smallDict.addWord("HElLo");
+    smallDict.addWord("help");
+    smallDict.addWord("a");
+    smallDict.addWord("subsequent");
+    smallDict.size();
+}
+
 }
